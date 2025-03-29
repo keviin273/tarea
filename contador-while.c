999b5b7516eb8369f538d13829c6b52ae3603b64
keviin273 <keviin273xd@gmail.com>
2025-03-29 16:22:11 -0300

    Add files via upload

---
+++ contador-while.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() 
+{
+	int numero,positivo;
+	positivo = 0;
+	for (int i = 1; i <= 20; i++)
+	{
+		printf("ingrese un numero: ");
+		scanf("%d",&numero);
+		if (numero > 0)
+		{
+		  positivo++;
+		}
+	}
+	printf("la cantidad de numeros positivos ingresados es %d",positivo);
+	return 0;
+}
+
